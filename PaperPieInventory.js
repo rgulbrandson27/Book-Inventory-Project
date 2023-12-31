8b95a5daac801cad2131a4e6d4064bf2b1aa997e
rgulbrandson27 <105253984+rgulbrandson27@users.noreply.github.com>
2023-12-30 22:29:30 -0600

    mvp for assignment - so much I else I would do

--- PaperPieInventory.js
+++ PaperPieInventory.js
@@ -1,17 +1,17 @@
 class Item {
-    constructor(title, price, minAge, maxAge, notes) {
+    constructor(title, price) {
         this.title = title;
         this.price = price;
-        this.minAge = minAge;
-        this.maxAge = maxAge;
-        this.notes = notes;
     }
 
     describeItem() {
-        return `Title: ${this.title}
+        return `
+        Count: ${this.numStocked}
+        Title: ${this.title}
         Price: ${this.price}
-        Ages: ${this.minAge} to ${this.maxAge}
-        Notes: ${this.notes}`;
+        Ages:  ${this.minAge} to ${this.maxAge}
+        Notes: ${this.notes}
+        Ref#:  ${this.itemNum}`;
     }
 }
 
@@ -21,129 +21,155 @@ class Category {
         this.items = [];
     }
 
-    addItem(item) {
-        if (item instanceof Item) {
-            this.items.push(item);
-         }  else {
-            throw new Error(`${item} is not yet a listed item`);
-        }
+    describeCategory() {
+        return `Category contains ${this.selectedCategory.items.length} items.`;
     }
-    describe() {
-        return `Category: ${this.categoryName} has ${this.items.length} items.`;
+
+    listItems(items) {
+        for (i=0; i < items.length; i++)
+            this.item.describeItem();
     }
 }
+
 class Menu {
     constructor() {
-        this.categories = [];    
+        this.categoriesList = [];    
         this.selectedCategory = null;  
     }
 
-
     start() {
-        let selection = this.showMainMenuOptions();
+        let selection = this.displayMainMenu();
+
         while (selection != 5) {                        
             switch (selection) {
                 case '1':
                     this.createNewCategory();
                     break;
                 case '2':
-                    this.viewCategory();
+                    this.displayCategories();
                     break;
                 case '3':
-                    this.deleteCategory();
+                    this.manageCategory();
                     break;
                 case '4':
-                    this.displayCategories();
+                    this.deleteCategory();
                     break;
                 case '5':
                     this.exitProgram();
                 default:
                     selection = 5;
             }
-            selection = this.showMainMenuOptions();
+            selection = this.displayMainMenu();   //this is called again while still in the loop
         }
-
         alert('Program Exited');
     }
     
-    showMainMenuOptions() {
+    displayMainMenu() {
         return prompt(`
-            1) Create New Category
-            2) View Category
-            3) Delete Category
-            4) Display All Categories
-            5) Exit Program
+            1)  Create New Category
+            2)  Display Categories
+            3)  Manage Category
+            4)  Delete Category
+            5)  Exit Program
         `);
     }
 
-    showCategoryMenuOptions(categoryInfo) {
+    displayCategoryMenu(categoryInfo) {
         return prompt(`
-        1) Create item
-        2) Delete item
-        0) Back
-        --------------------------
-        ${categoryInfo}
-        `);
+                1) Add item
+                2) Delete item
+                3) Return to Main Menu
+        _______________________________________
+        
+        ${categoryInfo}`);
     }
 
     createNewCategory() {
         let categoryName = prompt('Enter new category name:');
-        this.categories.push(new Category(categoryName));
-        console.log(this.categories);
+        this.categoriesList.push(new Category(categoryName, this.categoriesList.length + 1));
+        console.log(this.categoriesList);
         console.log(categoryName);
     }
 
-    viewCategory() {
-        let index = prompt('Enter the category you wish to view:');
-        if ((index > -1) && index < this.categories.length) {
-            this.selectedCategory = this.categories[index];
-            let description = 'Category Name: ' + this.selectedCategory.categoryName + '\n';
-            description += ' ' + this.selectedCategory.describe() + '\n';
-            for (let i = 0; i < this.selectedCategory.items.length; i++) {
-                    description += i + ') ' + this.selectedCategory.items[i].describeItem() + '\n';
-                }
-                let selection1 = this.showCategoryMenuOptions(description);
-                switch (selection1) {
-                    case '1':
-                        this.createItem();
-                        break;
-                    case '2':
-                        this.deleteItem();
-                }
-            }
-        }
-
     displayCategories() {
-        let categoryString = '';
-        for (let i = 1; i < this.categories.length; i++) {
-            console.log(this.categories[i]);
-        categoryString =+i + ') ' + this.categories[i].categoryName + '\n';
+        let categoriesList = "";
+        for (let i = 0; i < this.categoriesList.length; i++) { 
+        categoriesList += `
+        ${i+1} ) ${this.categoriesList[i].categoryName}`
         }
-        alert(categoryString);
+        alert(categoriesList);
     }
 
-    deleteCategory() {
-        let index = prompt('Enter the category you wish to delete: ');
-        if (index > -1 && index < this.categories.length) {
-            this.categories.splice(index, 1);
+    manageCategory() {         
+        let number = prompt("Enter the number of the category you wish to manage:");
+        if ((number > 0) && number <= this.categoriesList.length) {
+            this.selectedCategory = this.categoriesList[number - 1];
+            let description = "CATEGORY:  " + this.selectedCategory.categoryName + '\n';
+
+            for (let i = 1; i <= this.selectedCategory.items.length; i++) {
+                description += i + ") " + this.selectedCategory.items[i-1].title 
+                + " - " + this.selectedCategory.items[i-1].price + '\n';
+            }
+
+            let selection1 = this.displayCategoryMenu(description);
+            switch (selection1) {
+                case '1':
+                    this.createItem();
+                    break;
+                case '2':
+                    this.deleteItem();
             }
         }
-          
+    }   
+
+    // manageCategory() {         
+    //     let number = prompt("Enter the number of the category you wish to manage:");
+    //     if ((number > 0) && number <= this.categoriesList.length) {
+    //         this.selectedCategory = this.categoriesList[number - 1];
+    //         let itemsList = `
+    //         ITEMS LIST:
+
+    //         ${for (let i = 1; i <= this.selectedCategory.items.length; i++) {
+    //             itemsList += i} this.selectedCategory.items[i-1].title this.selectedCategory.items[i-1].price}
+            
+    //         `
+
+    //         let selection1 = this.displayCategoryMenu(this.selectedCategory.categoryName, itemsList);
+    //         switch (selection1) {
+    //             case '1':
+    //                 this.createItem();
+    //                 break;
+    //             case '2':
+    //                 this.deleteItem();
+    //         }
+    //     }
+    // }   
+    
+    deleteCategory() {
+        let index = prompt('Enter the category number you wish to delete:');
+        if (index > 0 && index <= this.categoriesList.length) {
+            this.categoriesList.splice((index-1), 1);         
+        }
+    }
+        //create "Are you sure" alert.  
+
+
     createItem() {
         let title = prompt('Enter new Title:');
         let price = prompt('Enter Price:')
-        let minAge = prompt('Enter min. age:')            
-        let maxAge = prompt('Enter max. age:')
-        let notes = prompt('Enter notes:')
-        this.selectedCategory.items.push(new Item(title, price, minAge, maxAge, notes));
+        // let minAge = prompt('Enter min. age:')            
+        // let maxAge = prompt('Enter max. age:')
+        // let notes = prompt('Enter notes:')
+        this.selectedCategory.items.push(new Item(title, price));
         }
 
     deleteItem() {
-        let index = prmopt('Enter the index of the item you wish to delete:');
-        if (index > -1 && index < this.selectedCategory.items.length) {
-            this.selectedCategory.items.splice(index, 1);
+        let index = prompt('Enter the number of the item you wish to delete:');
+        if (index > 0 && index <= this.selectedCategory.items.length) {
+            this.selectedCategory.items.splice(index-1, 1);
         }
     }
 }
+
 let menu = new Menu();
 menu.start();
